47dbdd643a3df1596aea1cc5bbed3b164c651635
Carl Enlund <carl.enlund@gmail.com>
2026-02-07 17:57:24 +0100

    images

--- documentation/image2.py
+++ documentation/image2.py
@@ -16,7 +16,7 @@ import argparse
 
 # Constants, these are the main "settings" for the image
 WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 1024, 128, 1
-FONT_PATH = "fonts/ttf/RadioCanadaDisplay-Regular.ttf"
+FONT_PATH = "fonts/ttf/Geomini-800.ttf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
@@ -25,7 +25,7 @@ LINE_ONE = "ABCDEFGHIJKLMNOPQ"
 LINE_TWO = "RSTUVWXYZ123456789"
 LINE_THREE = "abcdefghijklmnopqrstu"
 LINE_FOUR = "vwxyz,.;:!@#$%^&*(){}[]"
-BIG_TEXT_FONT_SIZE = 160
+BIG_TEXT_FONT_SIZE = 140
 BIG_TEXT_SIDE_MARGIN = MARGIN * 1
 BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5.45
 
